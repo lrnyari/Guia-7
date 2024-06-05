ca3666b8901a8a789c2f8f0391c1dd0a8a374d56
Lisandro <lisandronyari@gmail.com>
2024-06-05 13:24:34 -0300

    hasta el 5.1

--- guia7.py
+++ guia7.py
@@ -1,8 +1,5 @@
-def imprimir_hola_mundo() -> None:
-    hola_mundo: str = "Hola mundo!"
-    print(hola_mundo)
-
-
+def es_vocal(letra: str) -> bool:
+    return letra == 'a' or letra == 'A' or letra == 'e' or letra == 'E' or letra == 'i' or letra == 'I' or letra == 'o' or letra == 'O' or letra == 'u' or letra == 'U' 
 #EJ1.1
 #def pertenece(valor: int,lista: list[int]) -> bool:
 #    res: bool = False
@@ -111,12 +108,151 @@ def fortaleza_contraseña(palabra: str) -> str:
     else:
         return res[1]    
 
+#EJ 1.8
+tuple
+def saldo_actual(movimientos: list[(str, int)]) -> int:
+    saldo: int = 0
+    for i in range(len(movimientos)):
+        operacion = movimientos[i]
+        if(operacion[0] == 'I'):
+            saldo = saldo + operacion[1]
+        else:
+            saldo = saldo - operacion[1]
+    return saldo
+
+movimientos_prueba = [("I", 2000), ("R", 20),("R", 1000),("I", 300)]
+print(saldo_actual(movimientos_prueba))
+
+#EJ 1.9
+
+#
+
+#EJ 2.1
+def borrar_pares(lista: list[int]) -> list[int]:
+    for i in range(len(lista)):
+        if(lista[i] % 2 == 0):
+            lista[i] = 0
+    return lista
+print(borrar_pares([1,2,3,4,5]))
+
+#EJ 2.2
+
+def borrar_pares_in(lista: list[int]) -> list[int]:
+    lista_resultado: list[int] = []
+    for i in range(len(lista)):
+        if(lista[i] % 2 == 0):
+            lista_resultado.append(0)
+        else: 
+            lista_resultado.append(lista[i])
+    return lista_resultado
+
+print(borrar_pares_in([1,2,3,4,5]))
+
+#EJ 2.3
+
+def sacar_vocales(texto: list[str]) -> list[str]:
+    texto_resultado: list[str] = []
+    for i in range(len(texto)):
+        if(not es_vocal(texto[i])):
+            texto_resultado.append(texto[i])
+    return texto_resultado
+
+print(sacar_vocales("siento por ciento"))
+
+#EJ 2.4
+
+def reemplaza_vocales(texto: list[str]) -> list[str]:
+    texto_resultado: list[str] = []
+    for i in range(len(texto)):
+        if(es_vocal(texto[i])):
+            texto_resultado.append(" ")
+        else:
+            texto_resultado.append(texto[i])
+    return texto_resultado
+            
+print(reemplaza_vocales("ciento por ciento"))
+
+#EJ 2.5
+
+def dar_vuelta_str(texto: list[str]) -> list[str]:
+    texto_resultado: list[str] = []
+    for i in range(len(texto)):
+        texto_resultado.append(texto[len(texto) - i - 1])
+    return texto_resultado
+
+#EJ 2.6
+
+def sacar_repetidos(texto: list[str]) -> list[str]:
+    texto_resultado: list[str] = []
+    for i in range(len(texto)):
+        if(not texto[i] in texto_resultado):
+            texto_resultado.append(texto[i])
+    return texto_resultado
+
+print(sacar_repetidos("ciento por ciento"))
+
+#EJ 3
+def promedio_alumno(notas: list[int]) -> int:
+    resultado: int = 0
+    promedio: float
+    total: int = 0
+    for i in notas:
+        total = total + i
+    promedio = total / (len(notas) + 1)
+    for i in notas:
+        if(i<4 or promedio < 4):
+            resultado = 3
+    if(resultado != 3):
+        if(promedio > 7):
+            resultado = 1
+        else:
+            resultado = 2
+    return resultado
+
+print(promedio_alumno([5,6, 4, 3 ,6]))
+print(promedio_alumno([10,10,10 , 10]))
+
+#EJ 4.1
+
+def construir_estudiantes() -> list[str]:
+    resultado: list[str] = []
+    nombre: str = input('Ingrese nombre del estudiante. Para terminar, ingrese listo:\n')
+    while(nombre != "listo"):
+        resultado.append(nombre)
+        nombre = input('Ingrese nombre del estudiante. Para terminar, ingrese listo:\n')
+    return resultado
+
+#estudiantes_prueba = construir_estudiantes()
+#print(estudiantes_prueba)
+
+#EJ 4.2
+def historial_monedero() -> list[(str, int)]:
+    operacion: str = input('Qué operación desea realizar? Para terminar ingrese X:\n')
+    monto: int
+    historial: list[(str, int)] = []
+    while(operacion != 'X'):
+        monto = input('Ingrese el monto:\n')
+        historial.append((operacion, monto))
+        operacion: str = input('Qué operación desea realizar? Para terminar ingrese X:\n')
+    return historial
 
 
+# EJ 5.1
 
+def pertenece_a_cada_uno_v1(lista: list[list[int]], valor: int) -> list[bool]:
+    resultado: list[bool] = []
+    
+    for i in range(len(lista)):
+        if(valor in lista[i]):
+            resultado.append(True)
+        else:
+            resultado.append(False)
+    
+    return resultado
 
-
-
+prueba_ej5: list[list[int]] = [[1,2,3],[6,3,4],[2,4]]
+print(pertenece_a_cada_uno_v1(prueba_ej5,2))
+    
 #prueba: list[int] = [1,2,3,4]
 #prueba_str: list[str] = ["cinco","cincose","tres"]
 prueba_pass: str = 'contrasenia'
@@ -127,3 +263,5 @@ prueba_pass: str = 'contrasenia'
 #print(ordenados(prueba))
 #print(long_mayor_7(prueba_str))
 print(fortaleza_contraseña(prueba_pass))
+
+range(1,10,2)
